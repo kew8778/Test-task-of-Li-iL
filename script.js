da7f8c3815d80d314ecf7fc2aa4239880a087da6
Evgeniy Kogutich <kew8778@mail.ru>
2024-02-16 12:11:15 +0300

    тест сервера pages (git), вынес json

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ showList();
  * Вывод списка на страницу
  */
 async function showList() {
-  const arr = (await getResponceServer()).services; // массив 'services' из данных серевера
+  const arr = (await getResponceServer('/data.json')).services; // массив 'services' из данных серевера
   const lists = getObjLists(arr); // объект отсортированных списков
   const priceList = document.querySelector('.prizeList'); // родительский блок списка
 
@@ -134,132 +134,12 @@ function getArrowRight() {
 
 /**
  * Запрос на сервер (имитация)
+ * @param url {String} url запроса
  * @returns {Promise<void>}
  */
-async function getResponceServer() {
-  const response = await setResponceServer(); // await fetch(url запроса); - если GET
-  const res = await JSON.parse(response); // await response.json();
+async function getResponceServer(url) {
+  const response = await fetch(url);
+  const res = await JSON.parse(response);
 
   return res;
-}
-
-/**
- * Ответ сервера (для тестинга)
- * @returns {Promise<void>} JSON
- */
-function setResponceServer() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(JSON.stringify(
-        {
-          "services": [
-            {
-              "id": 101,
-              "head": 2,
-              "name": "Лечение кариеса",
-              "node": 1,
-              "price": 0.0,
-              "sorthead": 1
-            },
-            {
-              "id": 102,
-              "head": 101,
-              "name": "Пломбы",
-              "node": 1,
-              "price": 0.0,
-              "sorthead": 30
-            },
-            {
-              "id": 103,
-              "head": 102,
-              "name": "Фотополимер",
-              "node": 0,
-              "price": 1000.0,
-              "sorthead": 20
-            },
-            {
-              "id": 1,
-              "head": null,
-              "name": "Проф.осмотр",
-              "node": 0,
-              "price": 100.0,
-              "sorthead": 20
-            },
-            {
-              "id": 2,
-              "head": null,
-              "name": "Хирургия",
-              "node": 1,
-              "price": 0.0,
-              "sorthead": 10
-            },
-            {
-              "id": 3,
-              "head": 2,
-              "name": "Удаление зубов",
-              "node": 1,
-              "price": 0.0,
-              "sorthead": 10
-            },
-            {
-              "id": 4,
-              "head": 3,
-              "name": "Удаление зуба",
-              "node": 0,
-              "price": 800.0,
-              "sorthead": 10
-            },
-            {
-              "id": 5,
-              "head": 3,
-              "name": "Удаление 8ого зуба",
-              "node": 0,
-              "price": 1000.0,
-              "sorthead": 30
-            },
-            {
-              "id": 6,
-              "head": 3,
-              "name": "Удаление осколка зуба",
-              "node": 0,
-              "price": 2000.0,
-              "sorthead": 20
-            },
-            {
-              "id": 7,
-              "head": 2,
-              "name": "Хирургические вмешательство",
-              "node": 0,
-              "price": 200.0,
-              "sorthead": 10
-            },
-            {
-              "id": 8,
-              "head": 2,
-              "name": "Имплантация зубов",
-              "node": 1,
-              "price": 0.0,
-              "sorthead": 20
-            },
-            {
-              "id": 9,
-              "head": 8,
-              "name": "Коронка",
-              "node": 0,
-              "price": 3000.0,
-              "sorthead": 10
-            },
-            {
-              "id": 10,
-              "head": 8,
-              "name": "Слепок челюсти",
-              "node": 0,
-              "price": 500.0,
-              "sorthead": 20
-            }
-          ]
-        }
-      ));
-    }, 100);
-  });
 }
